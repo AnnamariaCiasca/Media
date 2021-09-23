0a0542a8e7a92c4670c8f7b6424c985e3035d7fb
Annamaria Ciasca <annamaria.ciasca@avanade.com>
2021-09-23 09:36:05 +0200

    Aggiungere i file di progetto.

---
+++ Media/GeneratoreEvento.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Media
+{
+   public class GeneratoreEvento
+    {
+        public delegate void GestoreEventi(GeneratoreEvento gen, double media);
+
+        public event GestoreEventi MediaSuperata;
+
+        public void SuperamentoMedia(double media)
+        {
+          if(MediaSuperata != null)
+            {
+                MediaSuperata(this, media);
+            }
+        }
+    
+    
+    
+    }
+}
---
+++ Media/Program.cs
@@ -0,0 +1,45 @@
+﻿//Realizzare un programma che permetta l’inserimento di una
+//serie di valori da tastiera, segnalando quando il valore medio
+//corrente scende al di sotto di una certa soglia
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Media
+{
+    class Program
+    {
+        static List<int> listaNumeri = new List<int>();
+        static void Main(string[] args)
+        {
+            GeneratoreEvento gen = new GeneratoreEvento();
+            gen.MediaSuperata += RicezioneNotifica;
+            while (true)
+            {
+                bool conversioneRiuscita = int.TryParse(Console.ReadLine(), out int num);
+                if (conversioneRiuscita)
+                {
+                    listaNumeri.Add(num);
+                    double media = CalcolaMedia(listaNumeri);
+                    if (media < 10)
+                    {
+                        gen.SuperamentoMedia(media);
+                    }
+                }
+            }
+        }
+
+        private static double CalcolaMedia(List<int> listaNumeri)
+        {
+            return listaNumeri.Sum() / listaNumeri.Count();
+        }
+
+        public static void RicezioneNotifica(GeneratoreEvento gen, double media)
+        {
+            Console.WriteLine($"La media ha un valore pari a {media} inferiore alla soglia critica");
+
+        }
+    }
+}
